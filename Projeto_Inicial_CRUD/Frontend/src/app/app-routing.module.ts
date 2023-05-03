a1cde2468b3eb41ed21ad3af4befdb7f08c6ba5d
Vitoria Mercado <andrader.vitoria@gmail.com>
2023-05-02 23:21:09 -0300

    Finalizado Projeto_Inicial_CRUD com Angular

--- Projeto_Inicial_CRUD/Frontend/src/app/app-routing.module.ts
+++ Projeto_Inicial_CRUD/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+
+const routes: Routes = [
+  {
+  // Sempre que estiver no path raiz, navegue para HomeComponent
+  path: "",
+  component: HomeComponent
+  },
+  {
+    // Sempre que estiver no path products, navegue para ProductCrudComponent
+    path: "products",
+    component: ProductCrudComponent
+  },
+  {
+    // Sempre que estiver no path create, navegue para ProductCreateComponent
+    path: "product/create",
+    component: ProductCreateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Projeto_Inicial_CRUD/Frontend/src/app/app.module.ts
+++ Projeto_Inicial_CRUD/Frontend/src/app/app.module.ts
@@ -8,18 +8,33 @@ import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavegacaoComponent } from './components/template/navegacao/navegacao.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { MatCardModule } from '@angular/material/card';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavegacaoComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
